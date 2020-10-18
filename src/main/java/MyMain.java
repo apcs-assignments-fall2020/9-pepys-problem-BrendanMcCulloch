9d6b8f188053b5df6aec58ae0e5f6a692d6f1fcf
BrendanMcCulloch <22bmcculloch@athenian.org>
2020-10-18 03:02:03 +0000

    resubmission

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,26 +5,72 @@ public class MyMain {
     // Returns in the answer as a double corresponding to the percentage
     // For example, 75.5% would be 75.5
     public static double probabilityOneSix() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int b = 0;
+        double y = 0.0;
+        for (int i = 0; i < 10000; i++){
+            for (int j = 0; j < 6; j++){
+                b = (int) ((Math.random() * 6) + 1);
+                if (b == 6){
+                    y += 1;
+                    break;
+                } 
+            }
+        }
+        double ou = (y / 10000.0) * 100.0;
+        return ou;
     }
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int b = 0;
+        double y = 0.0;
+        int u = 0;
+        for (int i = 0; i < 10000; i++){
+            u = 0;
+            for (int j = 0; j < 12; j++){
+                b = (int) ((Math.random() * 6) + 1);
+                if (b == 6){
+                    u += 1;
+                }
+                if (u == 2){
+                    y += 1;
+                    break;
+                }
+            }
+        }
+        double oooo = (y / 10000.0) * 100.0;
+        return (oooo);
     }
 
     // Calculate the probability of rolling at least three 6's when rolling 
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int b = 0;
+        double y = 0.0;
+        int u = 0;
+        for (int i = 0; i < 10000; i++){
+            u = 0;
+            for (int j = 0; j < 18; j++){
+                b = (int) ((Math.random()*6) + 1);
+                if (b == 6){
+                    u += 1;
+
+                }
+                if (u == 3){
+                    y += 1;
+                    break;
+                }
+            }
+        }
+        double ooui = (y / 10000.0) * 100.0;
+        return (ooui);
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        System.out.println("Probability of one six in 6 rolls " + probabilityOneSix());
+        System.out.println("probability of 2 sixes in 12 rolls " + probabilityTwoSixes());
+        System.out.println("probability of 3 sixes in 18 rolls " + probabilityTwoSixes());
     }
 }
